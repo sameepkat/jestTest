e1f60b4fca46209507c6cec96a119b90641554b8
Sameep <sameepsk2@gmail.com>
2025-01-14 10:59:18 +0545

    Test case complete

--- app.test.ts
+++ app.test.ts
@@ -16,16 +16,16 @@ describe('string operations', () => {
 describe('Math operations', () => {
 	describe('Calculator Object',() => {
 		test('add', () => {
-			expect(Calculator.sum(5+3)).toBe(8);
+			expect(Calculator.sum(5,3)).toBe(8);
 		})
 		test('subtract', () => {
-			expect(Calculator.sub(5-3)).toBe(2);
+			expect(Calculator.sub(5,3)).toBe(2);
 		})
 		test('divide', () => {
-			expect(Calculator.div(10/5)).toBe(2);
+			expect(Calculator.div(10,5)).toBe(2);
 		})
 		test('multiply', () => {
-			expect(Calculator.mul(10*5)).toBe(50);
+			expect(Calculator.mul(10,5)).toBe(50);
 		})
 	})
 	
--- app.ts
+++ app.ts
@@ -15,15 +15,48 @@ export function sum(a: number | string, b: typeof a) {
 }
 
 export const Calculator = {
-
+  sum: (a: number, b: number) => {
+      return a+b;
+  },
+  sub: (a: number, b: number) => {
+      return a-b;
+  },
+  div: (a: number, b: number) => {
+      return a/b;
+  },
+  mul: (a: number, b: number) => {
+      return a*b;
+  },
 };
 
-export function caesarCipher(input: string, shiftFactor: number){}
-
+export function caesarCipher(input: string, shiftFactor: number){
+  let caesar: string = "";
+  for(let i=0;i<input.length; i++){
+    let letterAscii = input.charCodeAt(i);
+    if(letterAscii >= 65 && letterAscii <=90)
+      caesar += String.fromCharCode(65+(letterAscii - 65 + shiftFactor)%26);
+    else if(letterAscii >= 97 && letterAscii <=122)
+      caesar += String.fromCharCode(97+(letterAscii - 97 + shiftFactor)%26);
+    else
+      caesar += String.fromCharCode(letterAscii);
+  }
+  return caesar;
+}
 interface analyzedArray {
   average: number,
   min: number,
   max: number,
   length: number
 }
-export function analyzeArray(inputArr: Array<number>):analyzedArray {}
+export function analyzeArray(inputArr: Array<number>){
+  const average = inputArr.reduce((sum, curr) => sum+curr, 0)/inputArr.length;
+  const min = Math.min(...inputArr);
+  const max = Math.max(...inputArr);
+  const length = inputArr.length;
+  return{
+    average,
+    min,
+    max,
+    length
+  }
+}
